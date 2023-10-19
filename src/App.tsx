589363b0444fd6ac6c24e260e6265f6b087d6906
Marco Lupo <marcolupo@Marcos-MacBook-Air.local>
2023-10-19 16:19:57 +0100

    convert useEffect to a reusable async await function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { CurrencyForm } from './components/CurrencyForm';
 import { currencies } from './utils/currencies';
@@ -12,30 +12,53 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [topCurrency, setTopCurrency] = useState<Currency>({ name: 'USD', rate: 0.0 });
-  const [bottomCurrency, setBottomCurrency] = useState<Currency>({ name: 'USD', rate: 0.0 });
+  const [topCurrency, setTopCurrency] = useState<Currency>({ name: 'USD', rate: 0 });
+  const [bottomCurrency, setBottomCurrency] = useState<Currency>({ name: 'USD', rate: 0 });
 
-  useEffect(() => {
-    // Don't make a request if the rate is 0
-    if (!topCurrency.rate) return setBottomCurrency({ ...bottomCurrency, rate: 0.0 });
-    // Simple GET request using fetch
-    fetch(
-      `${BASE_URL}?base_currency=${topCurrency.name}&apikey=${API_KEY}&currencies=${bottomCurrency.name}`
-    )
-      // Unwrap the response from the API from JSON
-      .then((response) => response.json())
-      //Extract the rate from the response
-      .then((response) => {
-        setBottomCurrency({
-          ...bottomCurrency,
-          rate: Number((response.data[bottomCurrency.name] * topCurrency.rate).toFixed(2))
-        });
-      });
-    // WHAT HAPPENS IF THERE IS AN ERROR?
-    // CAN WE HANDLE IT?
-  }, [topCurrency]);
+  // This will only work if the top currency changes
+  // useEffect(() => {
+  //   // Don't make a request if the rate is 0
+  //   if (!topCurrency.rate) return setBottomCurrency({ ...bottomCurrency, rate: 0.0 });
+  //   // Simple GET request using fetch
+  //   fetch(
+  //     `${BASE_URL}?base_currency=${topCurrency.name}&apikey=${API_KEY}&currencies=${bottomCurrency.name}`
+  //   )
+  //     // Unwrap the response from the API from JSON
+  //     .then((response) => response.json())
+  //     //Extract the rate from the response
+  //     .then((response) => {
+  //       setBottomCurrency({
+  //         ...bottomCurrency,
+  //         rate: Number((response.data[bottomCurrency.name] * topCurrency.rate).toFixed(2))
+  //       });
+  //     });
+  //   // WHAT HAPPENS IF THERE IS AN ERROR?
+  //   // CAN WE HANDLE IT?
+  // }, [topCurrency]);
 
   //DO WE ALSO WANT TO HANDLE WHEN THE BOTTOM CURRENCY CHANGES?
+  async function fetchCurrencyRate(
+    currency1: Currency,
+    currency2: Currency,
+    setCurrency2: (currency: Currency) => void
+  ) {
+    try {
+      if (!currency1.rate) return setCurrency2({ ...currency2, rate: 0 });
+      // Simple GET request using fetch
+      const response = await fetch(
+        `${BASE_URL}?base_currency=${currency1.name}&apikey=${API_KEY}&currencies=${currency2.name}`
+      );
+      // Unwrap the response from the API from JSON
+      const { data } = await response.json();
+
+      setCurrency2({
+        ...currency2,
+        rate: Number((data[currency2.name] * currency1.rate).toFixed(2))
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   return (
     <div
@@ -56,8 +79,20 @@ function App() {
           <span style={{ fontSize: '20px' }}>{currencies[bottomCurrency.name]}</span>
         </h1>
       </div>
-      <CurrencyForm currency={topCurrency} setCurrency={setTopCurrency} />
-      <CurrencyForm currency={bottomCurrency} setCurrency={setBottomCurrency} />
+      <CurrencyForm
+        currency={topCurrency}
+        currency2={bottomCurrency}
+        setCurrency={setTopCurrency}
+        setCurrency2={setBottomCurrency}
+        fetchCurrencyRate={fetchCurrencyRate}
+      />
+      <CurrencyForm
+        currency={bottomCurrency}
+        currency2={topCurrency}
+        setCurrency={setBottomCurrency}
+        setCurrency2={setTopCurrency}
+        fetchCurrencyRate={fetchCurrencyRate}
+      />
     </div>
   );
 }
--- src/components/CurrencyForm.tsx
+++ src/components/CurrencyForm.tsx
@@ -4,16 +4,35 @@ import { availableCurrencies } from '../utils/currencies';
 
 interface CurrencyFormProps {
   currency: Currency;
+  currency2: Currency;
   setCurrency: (currency: Currency) => void;
+  setCurrency2: (currency: Currency) => void;
+  fetchCurrencyRate: (
+    currency1: Currency,
+    currency2: Currency,
+    setCurrency2: (currency: Currency) => void
+  ) => void;
 }
 
-export const CurrencyForm: FC<CurrencyFormProps> = ({ currency, setCurrency }) => {
+export const CurrencyForm: FC<CurrencyFormProps> = ({
+  currency,
+  currency2,
+  setCurrency,
+  setCurrency2,
+  fetchCurrencyRate
+}) => {
   const handleNameChange = (event: { target: { value: string } }) => {
     setCurrency({ ...currency, name: event.target.value });
+    fetchCurrencyRate({ ...currency, name: event.target.value }, currency2, setCurrency2);
   };
 
   const handleRateChange = (event: { target: { value: string } }) => {
     setCurrency({ ...currency, rate: parseFloat(event.target.value) });
+    fetchCurrencyRate(
+      { ...currency, rate: parseFloat(event.target.value) },
+      currency2,
+      setCurrency2
+    );
   };
 
   return (
